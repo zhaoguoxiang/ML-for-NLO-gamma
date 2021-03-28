6f6698d52b1e1529c7a5857ca6ea8168073db59f
Guoxiang <mrzhaoguoxiang@outlook.com>
2021-03-28 16:56:28 +0800

    test

--- scp/sn2.py
+++ scp/sn2.py
@@ -1,75 +1,18 @@
-from urllib import request
-from openbabel import pybel
-from multiprocessing.spawn import import_main_path
-from multiprocessing import freeze_support
-from mordred import Calculator, descriptors
-import pandas as pd
-import numpy as np
-from rdkit import Chem
-from rdkit.Chem import AllChem 
-import cclib
 from pathlib import Path
 
-gammalogs = Path('./gamma/Sn2')
-gammafiles = list(gammalogs.rglob('*.log'))
+# import numpy as np
+# import pandas as pd
+# from matplotlib import pyplot as plt
 
-def log2xyz(logfiles):  
-    for logfile in logfiles:
-        data = cclib.io.ccread(str(logfile))
-        data.writexyz(str(logfile).rstrip('.log')+'.xyz')
+# from rdkit import Chem
+import cclib
 
-def xyz2mols(xyzfiles):
-    mols = []
-    for xyzfile in xyzfiles:
-        bel_mol = pybel.readfile('xyz',str(xyzfile)).__next__()
-        smi = smi = bel_mol.write('smi').split('\t')[0]
-        mol = Chem.MolFromSmiles(smi,sanitize=False)
-        mols.append(mol)
-    
-    return mols
-#log2xyz(gammafiles)
+logfiles_gamma_path = list(Path('./gamma/Sn2').rglob('*.log'))
+logfiles_gamma_name = [filename.stem for filename in logfiles_gamma_path]#this row and the next row can not be changed
+logfiles_gamma_str = [str(file) for file in logfiles_gamma_path]
+print(logfiles_gamma_str)
 
-xyzfiles = list(gammalogs.rglob('*.xyz'))
+def phasedata (logfile):
+    data = cclib.io.ccread(logfile)
 
-mols = xyz2mols(xyzfiles)
-
-if __name__ == "__main__":
-    freeze_support()
-    calc = Calculator(descriptors)
-    data = calc.pandas(mols)
-
-    namelist = []
-    for file in gammafiles:
-        name = file.stem
-        namelist.append(name)
-    namedict = {'name':namelist}
-    df = pd.DataFrame.from_dict(namedict)
-    n = pd.concat([data, df], axis=1)
-    print(n)
-
-
-
-
-#         bel_mol = pybel.readfile('xyz',str(logfile)).__next__()
-#         
-
-# def mdgen(mols):
-
-#     if __name__ == "__main__":
-#         freeze_support()
-#         calc = Calculator(descriptors)
-#         data = calc.pandas(mols)
-
-#     return data  
-
-# mols=log2mol(gammafiles)
-# data = mdgen(mols)
-
-# namelist = []
-# for file in gammafiles:
-#     name = file.split('\\')[-1].rstrip('.log')
-#     namelist.append(name)
-# namedict = {'name':namelist}
-# df = pd.DataFrame.from_dict(namedict)
-# n = pd.concat([data, df], axis=1)
-# print(n)
+pass
